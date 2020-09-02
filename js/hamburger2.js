251232e66d68428d3d68d5ae6f926b14fbd72da6
siddharthtank <62084107+siddharthtank@users.noreply.github.com>
2020-09-02 19:38:27 +0530

    Add files via upload

---
+++ js/hamburger2.js
@@ -0,0 +1,27 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll',()=>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = "#ffffff";
+        header.style["boxShadow"] = "0 -3px 10px gray";
+    }else{
+        header.style.backgroundColor = "transparent";
+        header.style["boxShadow"] = "none";
+    }
+});
+
+menu_item.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
